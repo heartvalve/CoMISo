d79bd4f6718ddc6fda5280ed95041c93e7f043ba
zimmer <zimmer@1355f012-dd97-4b2f-ae87-10fa9f823a57>
2009-08-28 16:29:33 +0000

    added licensing

--- trunk/CoMISo/Solver/MISolver.hh
+++ trunk/CoMISo/Solver/MISolver.hh
@@ -1,21 +1,37 @@
 /*===========================================================================*\
  *                                                                           *
- *                              CoMISo                                       *
+ *                               CoMISo                                      *
  *      Copyright (C) 2008-2009 by Computer Graphics Group, RWTH Aachen      *
- *                      www.graphics.rwth-aachen.de                          *
+ *                           www.rwth-graphics.de                            *
  *                                                                           *
- *---------------------------------------------------------------------------*
- *  This file is a part of CoMISo.                                          *
+ *---------------------------------------------------------------------------* 
+ *  This file is part of CoMISo.                                             *
  *                                                                           *
-\*===========================================================================*/
+ *  CoMISo is free software: you can redistribute it and/or modify           * 
+ *  it under the terms of the GNU Lesser General Public License as           *
+ *  published by the Free Software Foundation, either version 3 of           *
+ *  the License, or (at your option) any later version with the              *
+ *  following exceptions:                                                    *
+ *                                                                           *
+ *  If other files instantiate templates or use macros                       *
+ *  or inline functions from this file, or you compile this file and         *
+ *  link it with other files to produce an executable, this file does        *
+ *  not by itself cause the resulting executable to be covered by the        *
+ *  GNU Lesser General Public License. This exception does not however       *
+ *  invalidate any other reasons why the executable file might be            *
+ *  covered by the GNU Lesser General Public License.                        *
+ *                                                                           *
+ *  CoMISo is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU Lesser General Public License for more details.                      *
+ *                                                                           *
+ *  You should have received a copy of the GNU LesserGeneral Public          *
+ *  License along with CoMISo.  If not,                                      *
+ *  see <http://www.gnu.org/licenses/>.                                      *
+ *                                                                           *
+\*===========================================================================*/ 
 
-/*===========================================================================*\
- *                                                                           *
- *   $Revision: 1    $                                                       *
- *   $Author: zimmer $                                                       *
- *   $Date: 2009-08-05 16:35:37 +0200 (Wed, 05 Aug 2009) $                   *
- *                                                                           *
-\*===========================================================================*/
 
 
 
