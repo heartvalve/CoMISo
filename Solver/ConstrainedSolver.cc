6590f0aa6978e2fe994de33354284e6107d655d5
zimmer <zimmer@informatik.rwth-aachen.de>
2009-09-01 14:15:16 +0000

    licence fix

--- Solver/ConstrainedSolver.cc
+++ Solver/ConstrainedSolver.cc
@@ -1,36 +1,27 @@
 /*===========================================================================*\
-*                                                                           *
-*                               CoMISo                                      *
-*      Copyright (C) 2008-2009 by Computer Graphics Group, RWTH Aachen      *
-*                           www.rwth-graphics.de                            *
-*                                                                           *
-*---------------------------------------------------------------------------* 
-*  This file is part of CoMISo.                                             *
-*                                                                           *
-*  CoMISo is free software: you can redistribute it and/or modify           * 
-*  it under the terms of the GNU Lesser General Public License as           *
-*  published by the Free Software Foundation, either version 3 of           *
-*  the License, or (at your option) any later version with the              *
-*  following exceptions:                                                    *
-*                                                                           *
-*  If other files instantiate templates or use macros                       *
-*  or inline functions from this file, or you compile this file and         *
-*  link it with other files to produce an executable, this file does        *
-*  not by itself cause the resulting executable to be covered by the        *
-*  GNU Lesser General Public License. This exception does not however       *
-*  invalidate any other reasons why the executable file might be            *
-*  covered by the GNU Lesser General Public License.                        *
-*                                                                           *
-*  CoMISo is distributed in the hope that it will be useful,                *
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
-*  GNU Lesser General Public License for more details.                      *
-*                                                                           *
-*  You should have received a copy of the GNU LesserGeneral Public          *
-*  License along with CoMISo.  If not,                                      *
-*  see <http://www.gnu.org/licenses/>.                                      *
-*                                                                           *
+ *                                                                           *
+ *                               CoMISo                                      *
+ *      Copyright (C) 2008-2009 by Computer Graphics Group, RWTH Aachen      *
+ *                           www.rwth-graphics.de                            *
+ *                                                                           *
+ *---------------------------------------------------------------------------* 
+ *  This file is part of CoMISo.                                             *
+ *                                                                           *
+ *  CoMISo is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by     *
+ *  the Free Software Foundation, either version 3 of the License, or        *
+ *  (at your option) any later version.                                      *
+ *                                                                           *
+ *  CoMISo is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU General Public License for more details.                             *
+ *                                                                           *
+ *  You should have received a copy of the GNU General Public License        *
+ *  along with CoMISo.  If not, see <http://www.gnu.org/licenses/>.          *
+ *                                                                           *
 \*===========================================================================*/ 
+
 
 #include "ConstrainedSolver.hh"
 
