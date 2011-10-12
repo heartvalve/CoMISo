8d54a7745bcb5013025fef8fe3b72bfc4ca5446b
moebius <moebius@1355f012-dd97-4b2f-ae87-10fa9f823a57>
2011-10-12 12:12:40 +0000

    Updated global cmake finders for building MIQ with windows setup

--- trunk/CoMISo/cmake/FindCGAL.cmake
+++ trunk/CoMISo/cmake/FindCGAL.cmake
@@ -11,13 +11,31 @@ ENDIF (CGAL_INCLUDE_DIR)
 
 GET_FILENAME_COMPONENT(module_file_path ${CMAKE_CURRENT_LIST_FILE} PATH )
 
-# Look for the header file.
-FIND_PATH(CGAL_INCLUDE_DIR NAMES CGAL/auto_link/auto_link.h
-                           PATHS ../../External/include
-                                 "C:/Program Files/CGAL-3.4/include"
+IF (NOT APPLE )
+	# Look for the header file.
+	FIND_PATH(CGAL_INCLUDE_DIR NAMES CGAL/auto_link/auto_link.h
+        	                   PATHS ../../External/include
+                                 "C:/libs/CGAL-3.6/include"
+								 "C:/Program Files/CGAL-3.5/include"
+                                 "C:/Programme/CGAL-3.5/include"
+                                 "C:/libs/CGAL-3.5/include"
+                	             "C:/Program Files/CGAL-3.4/include"
                                  "C:/Programme/CGAL-3.4/include"
                                  "C:/libs/CGAL-3.4/include"
-                                 ${module_file_path}/../../../External/include)
+								 "C:/Programme/CGAL-3.9/include"
+								 "C:/Program Files/CGAL-3.9/include"
+								 "C:/Program Files (x86)/CGAL-3.9/include"
+	                             ${module_file_path}/../../../External/include)
+ELSE( NOT APPLE)
+	# Look for the header file.
+	FIND_PATH(CGAL_INCLUDE_DIR NAMES CGAL/auto_link/auto_link.h
+        	                   PATHS "/opt/local/include"
+                 )
+
+ENDIF( NOT APPLE )
+
+
+
 MARK_AS_ADVANCED(CGAL_INCLUDE_DIR)
 
 
@@ -36,10 +54,35 @@ IF(CGAL_INCLUDE_DIR )
                 PATHS "${CGAL_INCLUDE_DIR}/../lib"
                 DOC "Directory containing the CGAL library"
                ) 
+    add_definitions (-DCGAL_AUTOLINK)
+  ELSEIF( APPLE)
+    find_path(CGAL_LIBRARY_DIR 
+                NAMES "libCGAL.dylib"
+                PATHS "/opt/local/lib/"
+                DOC "Directory containing the CGAL library"
+               ) 
+    list ( APPEND CGAL_LIBRARIES CGAL CGAL_Core CGAL_ImageIO )
+    set (CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -frounding-math")
+    set (CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -frounding-math")
+
+  ELSE( WIN32 )
+    find_path(CGAL_LIBRARY_DIR
+              NAMES "libCGAL.so"
+              PATHS "/usr/lib/"
+              DOC "Directory containing the CGAL library"
+             )
+    list ( APPEND CGAL_LIBRARIES CGAL CGAL_Core CGAL_ImageIO CGAL_Qt4) 
+    set (CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -frounding-math")
+    set (CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -frounding-math")
+
+
+    # This is needed to link correctly against lapack
+    add_definitions (-DCGAL_USE_F2C)
   ENDIF(WIN32)
+
+
  
 
-  add_definitions (-DCGAL_AUTOLINK)
 ELSE(CGAL_INCLUDE_DIR )
   SET(CGAL_FOUND 0)
   SET(CGAL_INCLUDE_DIR)
